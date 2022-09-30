d041de59a41e43768bd76cf8c0ace9b1990bb761
Pellehakman <perhakman@outlook.com>
2022-09-30 03:02:02 +0200

    Update home.tsx

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -6,18 +6,25 @@ import SmallMatch from '../components/SmallMatch';
 import { Match } from '../models/data';
 import '../styles/Home.scss'
 
+// interface Props {
+//     matches: string[]
+    
+// };
 
-export default function Home() { 
+const Home: React.FC = () => {
     const [overlay, setOverlay] = useState<boolean>(false);
-    const [match, setMatch] = useState<string>("")
+    const [match, setMatch] = useState<Match[]>([])
     const [matches, setMatches] = useState<Match[]>([])
-    const [newUser, setNewUser] = useState<Match>({match: '', matchId: 0, win: true });
-    console.log('match', match)
+    
+    console.log('match', typeof match)
 
 
     const handleInput: (e:any) => void = (e:any) => {
-       
+        // const {name, value } = e.target;
+
         const yo = e.target.value
+       
+        // setMatch((prevUser: Match) => ({...prevUser,[name]: value}));  
         
         setMatch(yo)
 
@@ -28,15 +35,10 @@ export default function Home() {
 
 
     const handleSubmit = (e: React.FormEvent) => {
-
         e.preventDefault();
-
-    if (match) {
+        if (match) {
         setMatches([...matches, { matchId: Date.now(), match, win: false }]);
         setMatch("");
-
-        console.log(matches)
-        
     }
   };
   
@@ -46,21 +48,14 @@ export default function Home() {
 
     return (         
        <section className='home-container'>
-        
-
-       
 
         <button onClick={handleAddMatchOverlay}>ADD MATCH</button >
         {overlay && <AddMatch match={match} setMatch={setMatch} handleSubmit={handleSubmit} handleInput={handleInput} />}         
         <Matchlist matches={matches} setMatches={setMatches}  />
-
-
-
         
-       
-
-
        </section>
     )
 };
 
+
+export default Home
