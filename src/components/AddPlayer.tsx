ab709dc5efba81093db5878dd830883ce4a6c4ee
Pellehakman <perhakman@outlook.com>
2022-10-01 16:56:54 +0200

    next up teams

---
+++ src/components/AddPlayer.tsx
@@ -0,0 +1,28 @@
+
+import { ChangeEvent, useRef, useState } from 'react';
+import '../styles/Addplayer.scss'
+import { Match } from '../models/data'
+
+
+
+
+const AddPlayer = () => {
+
+
+
+
+    return(
+        <form>
+            <input placeholder='playerOne'/>
+        
+
+
+
+        
+    </form>
+    )
+    
+  
+
+    }
+ export default AddPlayer
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,13 +1,14 @@
 
-import { useEffect, useState } from 'react';
-import AddMatch from '../components/AddMatch';
+import { useState } from 'react';
+
 import Matchlist from '../components/Matchlist'
-import '../styles/AddMatch.scss'
+
 import SmallMatch from '../components/SmallMatch';
 import { actions as matchActions } from '../features/matchReducer'
 import { Match } from '../models/data';
 import '../styles/Home.scss'
 import useLocalStorage from '../components/hooks/use-local-storage';
+import AddPlayer from '../components/AddPlayer';
 
 
 interface Props {
@@ -16,16 +17,13 @@ interface Props {
 };
 
 const Home: React.FC<Props> = () => { 
-
-
-    
-
-
-
     const [overlay, setOverlay] = useState<boolean>(false);
-
     const [matches, setMatches] = useLocalStorage<Match[]>('matches',[])
+console.log(matches)
 
+    const [ handleSQ, setHandleSQ] = useState([])
+    
+    
 
 
 
@@ -34,9 +32,19 @@ const Home: React.FC<Props> = () => {
     const [teamTwo, setTeamTwo] = useState<string>("")
  
 
-   
+    const teamOneCopy = [...teamOne];
+    
+        if(teamOneCopy){
+            console.log('shit')
+            
+    };
 
-
+// GÖRA TEAMS i object
+// players
+// playerOneID>kills>deaths
+// playerOne>kills>deaths
+// playerOne>kills>deaths
+// playerOne>kills>deaths
 
 const handleTeamOneInput: (e:any) => void = (e:any) =>{setTeamOne(e.target.value)}
 const handleTeamTwoInput: (e:any) => void = (e:any) =>{setTeamTwo(e.target.value)}
@@ -44,12 +52,9 @@ const handleTeamTwoInput: (e:any) => void = (e:any) =>{setTeamTwo(e.target.value
 
    const handleSubmit: (e:any) => void = (e:any) => {
     e.preventDefault();
-    setMatches([...matches, { matchId: Date.now(), teamOne, teamTwo }].sort((a, b) => {
+    setMatches([...matches, { matchId: Date.now(), teamOne, teamTwo, teamOneCopy }].sort((a, b) => {
         return b.matchId - a.matchId;}));
-    
-  
-        
-  };
+    };
     
     
   
@@ -61,22 +66,12 @@ const handleTeamTwoInput: (e:any) => void = (e:any) =>{setTeamTwo(e.target.value
 
         <button onClick={handleAddMatchOverlay}>ADD MATCH</button >
         {overlay && 
-        <form
-        className="add-match-container" onSubmit={handleSubmit}>
-       <header>This is AddMatch</header>
-
-
-       <input type="text" id='teamOne' name='teamOne' placeholder='teamOne' required onChange={(e) => handleTeamOneInput(e)} />
-       <input type="text" id='teamTwo' name='teamTwo' placeholder='teamTwo' required onChange={(e) => handleTeamTwoInput(e)} />
-       <input type="submit" />
-
-
-
-
-       
-   </form>
-        
-        
+        <form className="add-match" onSubmit={handleSubmit}>
+            <header>This is AddMatch</header>
+            <input type="text" id='teamOne' name='teamOne' placeholder='teamOne' required onChange={(e) => handleTeamOneInput(e)} />
+            <input type="text" id='teamTwo' name='teamTwo' placeholder='teamTwo' required onChange={(e) => handleTeamTwoInput(e)} />
+            <input type="submit" />
+        </form>
         }         
         
         <Matchlist matches={matches} setMatches={setMatches}  />
@@ -93,6 +88,4 @@ const handleTeamTwoInput: (e:any) => void = (e:any) =>{setTeamTwo(e.target.value
 
 export default Home
 
-function dispatch(arg0: any) {
-    throw new Error('Function not implemented.');
-}
+
